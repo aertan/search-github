f39bb276927700266d2149c029bfcccbb8a0561a
Ahmet Ertan <ahmetertan@yandex.com>
2022-07-10 15:01:45 +0300

    Initiate RepoModule (Service + Controller)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RepoModule } from './repo/repo.module';
 
 @Module({
-  imports: [],
+  imports: [RepoModule],
   controllers: [AppController],
   providers: [AppService],
 })
