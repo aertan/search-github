9a0b6cc87bb10e35e8094f00ab6e0c7ac02f9457
Ahmet Ertan <ahmetertan@yandex.com>
2022-07-10 22:56:17 +0300

    Polish validation
    Input for "limit" must be one of the required values: 10, 50, 100

--- src/models/input-dto.model.ts
+++ src/models/input-dto.model.ts
@@ -1,9 +1,10 @@
-import { IsNotEmpty, IsNumberString } from 'class-validator';
+import { IsNotEmpty, IsNumberString, IsIn } from 'class-validator';
 
 export class InputDto {
 
     date: String;
 
+    @IsIn([10, 50, 100])
     @IsNotEmpty()
     @IsNumberString()
     limit: Number;
